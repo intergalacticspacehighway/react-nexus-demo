2417297d6c398cff57740fd0ff2e053f1ee50c19
Nishan <nishanbende@gmail.com>
2023-07-05 19:09:04 +0530

    demo 2 - add console.log

--- InitialiseJSRuntime.mm
+++ InitialiseJSRuntime.mm
@@ -10,8 +10,30 @@ using namespace facebook;
 
 - (void)initialiseJSRuntime {
   auto hermesRuntime = facebook::hermes::makeHermesRuntime();
-  auto INJECTED_SCRIPT = "let a = 1+2;";
+  
+  auto console = jsi::Object(*hermesRuntime);
+  console.setProperty(*hermesRuntime, "log",
+                      jsi::Function::createFromHostFunction(*hermesRuntime, jsi::PropNameID::forAscii(*hermesRuntime, "log"),
+                                                            1, [](jsi::Runtime& rt,
+                                                                   const jsi::Value& thisValue,
+                                                                   const jsi::Value* arguments,
+                                                                   size_t count) -> jsi::Value {
+    std::string elementType = arguments[0].asString(rt).utf8(rt);
+    NSString *nsStr = [NSString stringWithUTF8String:elementType.c_str()];
+    NSLog(@"%@", nsStr);
+
+    return jsi::Value::undefined();
+}));
+
+  hermesRuntime->global().setProperty(*hermesRuntime, "console", console);
+
+
+  auto INJECTED_SCRIPT = "let a = 1+2; console.log('hello react nexus '+a)";
+  
   hermesRuntime->evaluateJavaScript(std::make_unique<facebook::jsi::StringBuffer>(INJECTED_SCRIPT), "");
+
+
+
 }
 
 @end
